e07341704616d4fd217e1b48e19a7d45cd570a5d
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2023-02-06 22:08:02 +0100

    Dodaj pliki projektów.

---
+++ VinWin.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VinWin.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

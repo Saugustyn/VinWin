0e438e364bc42a2aa10854b89f340f34ff76532d
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2023-02-07 12:49:54 +0100

    Product API basic setup

---
+++ VinWin.Sevices.ProductAPI/DbContexts/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VinWin.Sevices.ProductAPI.Models;
+
+namespace VinWin.Sevices.ProductAPI.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}

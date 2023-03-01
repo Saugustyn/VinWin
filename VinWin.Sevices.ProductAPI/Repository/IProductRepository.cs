25c26bdf5c5656b88213aca3e01bf483d93f3261
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2023-03-01 12:56:28 +0100

    Product Repository Interface

--- VinWin.Sevices.ProductAPI/Repository/IProductRepository.cs
+++ VinWin.Sevices.ProductAPI/Repository/IProductRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace VinWin.Sevices.ProductAPI.Repository
+﻿using VinWin.Sevices.ProductAPI.Models.Dto;
+
+namespace VinWin.Sevices.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
     }
 }
